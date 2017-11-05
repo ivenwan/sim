37470acff7d1de1cb20bbb6285cb090864976692
lu <ivenwan@gmail.com>
2017-11-04 22:53:26 -0700

    Add memory class

--- cache.py
+++ cache.py
@@ -1,6 +1,37 @@
 import simpy
 import random
 
+
+class Memory(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+        self.data = []
+
+    def length(self):
+        return self.end - self.start +1
+
+    def linearize(self):
+        for i in range(self.start, self.end+1):
+            self.data.append(i)
+
+    def rand(self):
+        for i in range(0, self.length()):
+            self.data.append(random.randint(0,100))
+
+    def check(self,addr):
+        assert addr>=self.start and addr<=self.end
+
+
+    def read(self,addr):
+        self.check(addr)
+        return self.data[addr-self.start]
+
+    def write(self, addr, data):
+        self.check(addr)
+        self.data[addr-self.start] = data
+
+
 class Pool(object):
     def __init__(self):
         self.id = 0
@@ -91,6 +122,12 @@ class LoadStore(object):
 def main():
 
     env = MyEnv()
+    mem = Memory(256,1024)
+    mem.linearize()
+
+
+    print("mem has data %s" % mem.data)
+    mem.write(512,89)
 
     ls = LoadStore(env)
     print('start running')
