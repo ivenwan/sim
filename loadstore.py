060f9f5125f87c8d218e243b953f3764d15ce081
lu <ivenwan@gmail.com>
2017-11-12 21:17:05 -0800

    add packet class

--- loadstore.py
+++ loadstore.py
@@ -1,6 +1,7 @@
 import simpy
 import random
 
+LoadStoreCmd = ['ldr', 'str']
 
 class Memory(object):
     def __init__(self, start, end):
@@ -43,17 +44,17 @@ class Pool(object):
 
 
 class MSQ(object):
-    def __init__(self, env, num_units, pool):
+    def __init__(self, env, capacity, pool):
         self.env = env
-        self.arbiter = simpy.Resource(env, num_units)
-        #self.free_array = [1] * num_units  #0..num_units-1
-        #self.busy_array = [0] * num_units
-        self.free_list = list(range(0, num_units))
+        self.arbiter = simpy.Resource(env, capacity)
+        self.free_list = list(range(0, capacity))
         self.pool = pool
+        self.packet = [None] * capacity
 
-    def alloc(self):
+    def alloc(self, packet):
         assert self.free_list
         msq_id = self.free_list.pop()
+        self.packet[msq_id] = packet
         return msq_id
 
     def release(self, msq_id):
@@ -61,9 +62,10 @@ class MSQ(object):
         self.free_list.append(msq_id)
         return msq_id
 
-    def access(self, msqid):
+    def access(self, msq_id):
         hitL2 = random.uniform(0, 1) < 0.75
-        execstr = "MSQ(%d)#" % msqid
+        packet = self.packet[msq_id]
+        execstr = "MSQ(%d)-Addr(%08x)#" % (msq_id, packet.get_addr())
         lat = random.randint(8, 40)
         start = self.env.now
         yield self.env.timeout(lat)
@@ -74,26 +76,66 @@ class MSQ(object):
         return 0, execstr
 
 
+class Packet(object):
+    def __init__(self, pool):
+        self.cmd = None
+        self.addr = None
+        self.id = pool.draw()
+
+    def get_id(self):
+        return self.id
+
+    def rand_addr(self, start_addr, end_addr):
+        self.addr = random.randint(start_addr, end_addr)
+
+    def rand_cmd(self, cmd_list):
+        self.cmd = random.choice(cmd_list)
+
+    def get_addr(self):
+        return self.addr
+
+    def get_cmd(self):
+        return self.cmd
+
+
 class LoadStore(object):
     def __init__(self, env, msq, pool):
         self.env = env
         self.msq = msq
         self.pool = pool
 
-    def load(self):
+    def do(self, packet):
+        cmd = packet.get_cmd()
+        if cmd == 'ldr':
+            yield self.env.process(self.load(packet))
+        elif cmd == 'str':
+            yield self.env.process(self.store(packet))
+
+    def store(self, packet):
         print('@{0:5d} : '.format(self.env.now), end='')
-        id = self.pool.draw()
-        execstr = "ldr#%d" % id
+        id = packet.get_id()
+        addr = packet.get_addr()
+        execstr = "str::id=%d ::addr=%05x" % (id, addr)
         lat = 1
         execstr += "[%d]." % lat
-        data_arrival = self.env.process(self.accessL1(id))
+        yield self.env.timeout(lat)
+        print("%s" % execstr)
+
+    def load(self, packet):
+        print('@{0:5d} : '.format(self.env.now), end='')
+        id = packet.get_id()
+        addr = packet.get_addr()
+        execstr = "ldr::id=%d ::addr=%05x-" % (id, addr)
+        lat = 1
+        execstr += "[%d]." % lat
+        data_arrival = self.env.process(self.accessL1(id, packet))
         code, L1_execstr = yield data_arrival
         execstr += L1_execstr
         print("%s" % execstr)
 
-    def accessL1(self, id):
+    def accessL1(self, id, packet):
         prob = random.uniform(0, 1)
-        hitL1 = prob < 0.5
+        hitL1 = prob < 0.75
         id = self.pool.draw()
         execstr = "L1#%d" % id
 
@@ -109,7 +151,7 @@ class LoadStore(object):
             with self.msq.arbiter.request() as request:
                 yield request
                 # alloc a msq
-                msq_id = self.msq.alloc()
+                msq_id = self.msq.alloc(packet)
                 #print('@%d: msq req grant %d' % (self.env.now, msq_id))
                 L2 = self.env.process(self.msq.access(msq_id))
                 code, L2_execstr = yield L2
@@ -119,10 +161,13 @@ class LoadStore(object):
                 return (code, execstr)
 
 
-def setup(env, loadstore, num_transactions):
+def setup(env, pool, loadstore, num_transactions):
     for i in range(num_transactions):
+        packet = Packet(pool)
+        packet.rand_addr(100,500)
+        packet.rand_cmd(LoadStoreCmd)
         yield env.timeout(random.randint(0,2))
-        env.process(loadstore.load())
+        env.process(loadstore.do(packet))
         i += 1
 
 
@@ -138,7 +183,7 @@ ls = LoadStore(env, msq, pool)
 
 print('start running')
 
-env.process(setup(env,ls,1000))
+env.process(setup(env, pool, ls,1000))
 env.run(until=200000)
 
 
